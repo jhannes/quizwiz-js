24923b007c4cc0054255c561d8f346fbf3312573
Johannes Brodwall <johannes@brandmaster.com>
2014-11-16 06:15:59 +0100

    Normal server also needs to set up Sequelize!

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var app = require('./app');
 
-var resourceController = require('./server/resourceController');
-resourceController(app, '/api/questions', require('./server/questions')());
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(process.env.DATABASE_URL || 
+    "postgres://quizwiz:quizwiz@localhost:5432/quizwiz_dev",
+    {logging: false});
 
+var migrations = __dirname + "/migrations/";
+sequelize.getMigrator({path: migrations}).migrate().done(function() {
+  var resourceController = require('./server/resourceController');
+  resourceController(app, '/api/questions', require('./server/questions')(sequelize));
 
-var server = app.listen(3000, function() {
-  console.log(server.address());
+  var server = app.listen(process.env.PORT || 3000, function() {
+    console.log(server.address());
+  });
 });
+
